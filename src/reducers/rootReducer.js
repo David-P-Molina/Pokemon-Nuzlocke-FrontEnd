a053cd92bd96e34fe6e847dd17e3d4d656e67926
David Moina <davemolina777@gmail.com>
2021-10-02 19:25:10 -0500

    Import reducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux'
+import GamesReducer from './GamesReducer'
+import GraveyardReducer from './GraveyardReducer'
+import NuzlockeReducer from './NuzlockeReducer'
+import PokemonReducer from './PokemonReducer'
+import RulesReducer from './RulesReducer'
+import TeamsReducer from './TeamsReducer'
+import TrainersReducer from './TrainersReducer'
 
-const rootReducer = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const rootReducer = combineReducers({
+
+})
 
 export default rootReducer
