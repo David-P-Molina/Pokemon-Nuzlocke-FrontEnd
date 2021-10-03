129c929ee97b41a6392de8c6f3d5e73fd6b3780b
David Moina <davemolina777@gmail.com>
2021-10-02 19:38:34 -0500

    Add basic state, handleOnChange, and handleOnSubmit

--- src/components/Game/GameForm.js
+++ src/components/Game/GameForm.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react'
 
 class GameForm extends Component {
+    state = {
+        name: '',
+        releaseDate: '',
+        generation: '',//id
+        description: '',
+    }
+    handleOnChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    handleOnSubmit = (e) => {
+        e.preventDefault()
+
+        this.setState({
+            name: '',
+            releaseDate: '',
+            generation: '',//id
+            description: '',
+        })
+    }
     render() {
         return (
             <div>
