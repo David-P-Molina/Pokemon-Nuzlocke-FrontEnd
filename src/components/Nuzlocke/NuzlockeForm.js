ee3cf64a11033fe8746407c338203d6d7819ba3d
David Moina <davemolina777@gmail.com>
2021-10-02 19:31:00 -0500

    Layout basic structure for forms

---
+++ src/components/Nuzlocke/NuzlockeForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NuzlockeForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default NuzlockeForm
