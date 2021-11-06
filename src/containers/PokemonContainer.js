39fcc979a3a1d7e09e198ad32c9de634a7d01170
David Moina <davemolina777@gmail.com>
2021-11-06 13:04:39 -0500

    Import state through connect, impornt pokemon list

--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
+import PokemonList from '../components/Pokemon/PokemonList'
+import { connect } from 'react-redux'
 
 export class PokemonContainer extends Component {
     render() {
         return (
             <div>
-                
+                <PokemonList pokemon={this.props.pokemon}/>
             </div>
         )
     }
 }
-
-export default PokemonContainer
+const mapStateToProps = (state) => {
+    return {
+        pokemon: state.pokemon
+    }
+}
+export default connect(mapStateToProps)(PokemonContainer)\
