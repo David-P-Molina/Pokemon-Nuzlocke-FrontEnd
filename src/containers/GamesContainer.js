0e153b2b4c0877f5093fafd4dba337fd2026a9bf
David Moina <davemolina777@gmail.com>
2021-11-06 13:09:23 -0500

    Import state through connect, import games list and form

--- src/containers/GamesContainer.js
+++ src/containers/GamesContainer.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import GameForm from '../components/Game/GameForm'
+import GamesList from '../components/Game/GamesList'
+import { connect } from 'react-redux'
 
 export class GamesContainer extends Component {
     render() {
         return (
             <div>
-                
+                <GameForm />
+                <GamesList games={this.props.games} />
             </div>
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        games: state.games
+    }
+}
 
-export default GamesContainer
+export default connect(mapStateToProps)(GamesContainer)
