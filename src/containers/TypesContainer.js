11a97d2622cd0282a3ae24a6ab8583d7804a265c
David Moina <davemolina777@gmail.com>
2021-11-22 17:21:07 -0600

    convert const mapTypes from arrow function to const

--- src/containers/TypesContainer.js
+++ src/containers/TypesContainer.js
@@ -2,15 +2,10 @@ import React, { Component } from 'react'
 
 class TypesContainer extends Component {
     render() {
-        const mapTypes = () => {
-            debugger
-            this.props.types.map((type) => {
-                return <h1 key={type.name}>{type.name}</h1>
-            })
-        }
+        const mapTypes = this.props.types.map((type) => <h1 key={type.name}>{type.name}</h1>)
         return (
             <div>
-                {mapTypes()}
+                {mapTypes}
             </div>
         )
     }
