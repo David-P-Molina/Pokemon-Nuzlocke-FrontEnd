f226d11ff6529a3949814cbdb7f952af6d18f63e
David Moina <davemolina777@gmail.com>
2021-11-22 17:28:10 -0600

    format basic type information

--- src/containers/TypesContainer.js
+++ src/containers/TypesContainer.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react'
 
 class TypesContainer extends Component {
     render() {
-        const mapTypes = this.props.types.map((type) => <h1 key={type.name}>{type.name}</h1>)
+        const mapTypes = this.props.types.map((type) => {
+            return (
+                <>
+                <h3 key={type.name}>{type.name}</h3>
+                
+                    <li>{type.double_damage_from}</li>
+                
+                </>
+                )
+        })
+
         return (
             <div>
+                <h1>Pokemon Types</h1>
                 {mapTypes}
             </div>
         )
