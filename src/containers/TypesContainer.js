ed8c56e0d6d7243598f1a67e0c72197431b3e781
David Moina <davemolina777@gmail.com>
2021-11-21 18:36:16 -0600

    Create iterator to display info

--- src/containers/TypesContainer.js
+++ src/containers/TypesContainer.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react'
 
 class TypesContainer extends Component {
     render() {
+        const mapTypes = () => {
+            this.props.types.map((type) => {
+                <h1>{type.name}</h1>
+            })
+        }
         return (
             <div>
-                
+            {mapTypes()}
             </div>
         )
     }
 }
 
-export default TypesContaier
+export default TypesContainer
