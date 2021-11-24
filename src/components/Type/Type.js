c683c557d0aa7728ee2b24163affb0cd27a705a1
David Moina <davemolina777@gmail.com>
2021-11-23 23:14:09 -0600

    Pass information from list to single type component

--- src/components/Type/Type.js
+++ src/components/Type/Type.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Type = () => {
+const Type = ({ type }) => {
     return (
         <div>
+            <>
+            <h3 key={type.name}>{type.name}</h3>
             
+                <li>{type.double_damage_from}</li>
+            
+            </>
+            )
         </div>
     )
 }
