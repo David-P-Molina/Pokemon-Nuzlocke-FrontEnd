1e720b02e244a5faeb3972410b371378abc33bd4
David Moina <davemolina777@gmail.com>
2021-11-24 08:44:48 -0600

    Remove extra lines & tabs to format Types Component

--- src/components/Type/Type.js
+++ src/components/Type/Type.js
@@ -3,18 +3,13 @@ import React from 'react'
 const Type = ({ type }) => {
     return (
         <div>
-            <>
-            <h3 key={type.name}>{type.name}</h3>
-            
+            <h3 key={type.name}>{type.name}</h3>    
                 <li><strong>Double Damage From </strong>: {type.double_damage_from}</li>
                 <li><strong>Double Damage To </strong>: {type.double_damage_to}</li>
                 <li><strong>Half Damage From </strong>: {type.half_damage_from}</li>
                 <li><strong>Half Damage To </strong>: {type.half_damage_to}</li>
                 <li><strong>No Damage From</strong>: {type.no_damage_from}</li>
                 <li><strong>No Damage To </strong>: {type.no_damage_to}</li>
-            
-            </>
-            )
         </div>
     )
 }
