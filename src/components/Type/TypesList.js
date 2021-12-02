33bf0ca4f0acd8ed12380c67eb8a8188a0366292
David Moina <davemolina777@gmail.com>
2021-12-02 17:28:01 -0600

    Deconstruct props and add if statement if fetch fails to work

--- src/components/Type/TypesList.js
+++ src/components/Type/TypesList.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Type from './Type'
 
-const TypesList = () => {
-    const mapTypes = this.props.types.map((type) => <Type type={type} />)
+const TypesList = ({types}) => {
+    const mapTypes = () => types.map((type) => <Type type={type} />)
+    const renderTypes = () => {
+        if (types === []) {
+            return <h1>No Types</h1>
+        } else {
+           return mapTypes()
+        }
+    }
     return (
         <div>
-            {mapTypes}
+            {renderTypes()}
         </div>
     )
 }
