d7519417ebe27e3d7ad6494f44c6c3e77157d561
David Moina <davemolina777@gmail.com>
2021-12-06 22:43:53 -0600

    Import GenerationsList to container

--- src/containers/GenerationsContainer.js
+++ src/containers/GenerationsContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import GenerationsList from '../components/Generation/GenerationsList'
 
 class GenerationsContainer extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Pokemon Generations</h1>
+                <GenerationsList />
             </div>
         )
     }
