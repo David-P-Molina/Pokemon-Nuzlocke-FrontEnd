59f992f40c0eb7ad29df9ad588d74d7cef3bb809
David Moina <davemolina777@gmail.com>
2021-12-06 22:41:30 -0600

    Create basic generations container

---
+++ src/containers/GenerationsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class GenerationsContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default GenerationsContainer
