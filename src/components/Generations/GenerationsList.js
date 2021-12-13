470c4cc30e55f3866f2dd4ae763c4613bb94f6fc
David Moina <davemolina777@gmail.com>
2021-12-12 20:11:32 -0600

    Create renderGenerations() and loadingSpinner

--- src/components/Generations/GenerationsList.js
+++ src/components/Generations/GenerationsList.js
@@ -4,9 +4,16 @@ import LoadingSpinner from '../LoadingSpinner'
 
 const GenerationsList = ({ generations }) => {
     const mapGenerations = () => generations.map((gen) => <Generation generation={gen}/>)
+    const renderGenerations = () => {
+        if (generations === []) {
+            return <LoadingSpinner content="Generation Info" />
+        } else {
+            return mapGenerations()
+        }
+    }
     return (
         <div>
-            
+            {renderGenerations()}
         </div>
     )
 }
