493c7038a37d3d11c9537fb0d9379d0eb1e58cfe
David Moina <davemolina777@gmail.com>
2021-12-13 21:19:13 -0600

    Render Games list

--- src/components/Game/GamesList.js
+++ src/components/Game/GamesList.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import LoadingSpinner from '../LoadingSpinner'
+import Game from './Game'
 
 const GamesList = ({games}) => {
     const mapGames = () => games.map((game) => <Game game={game}/>)
+    const renderGames = () => {
+        if (games === []) {
+            return <LoadingSpinner content="Games" />
+        } else {
+            return mapGames()
+        }
+    }
     return (
         <div>
             <h1>Pokemon Games</h1>
+            {renderGames()}
         </div>
     )
 }
