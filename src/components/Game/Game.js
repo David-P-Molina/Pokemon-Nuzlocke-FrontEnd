e25472859e8296a63cc5b55de7d27f65e4e19452
David Moina <davemolina777@gmail.com>
2021-12-15 16:14:49 -0600

    Create structure for basic game component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Game = () => {
+const Game = ({ game }) => {
     return (
         <div>
-            
+            <h1>{game.name} <em>{game.release_date}</em>></h1> 
+            <p>{game.description}</p>
         </div>
     )
 }
