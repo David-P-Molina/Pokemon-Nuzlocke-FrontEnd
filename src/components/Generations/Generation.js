1b4ec99fd7d9dd9a8f6bf0672fff3d5cef8e5f6d
David Moina <davemolina777@gmail.com>
2021-12-15 16:17:15 -0600

    Create structure for basic generation component

--- src/components/Generations/Generation.js
+++ src/components/Generations/Generation.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const Generation = () => {
+const Generation = ({ generation }) => {
     return (
         <div>
-            
+            <h1>{generation.region}</h1>
+            <p>{generation.description}</p>
+            <h3>History</h3>
+            <p>{generation.history}</p>
         </div>
     )
 }
