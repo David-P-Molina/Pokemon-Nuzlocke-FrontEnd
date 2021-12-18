ebd56e7e1b0ad8dcf4955a07b21ad51f5ea76e06
David Moina <davemolina777@gmail.com>
2021-12-17 23:00:05 -0600

    Import graveyard to container

--- src/containers/GraveyardContainer.js
+++ src/containers/GraveyardContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Graveyard from '../components/Gravesyard/Graveyard'
 
 export class GraveyardContainer extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Username's Graveyard</h1>
+                <Graveyard />
             </div>
         )
     }
