594c9eab8497fdd3abfeb42e202eceeda20e113d
David Moina <davemolina777@gmail.com>
2021-12-17 23:03:57 -0600

    Import game and render games info in generation

--- src/components/Generations/Generation.js
+++ src/components/Generations/Generation.js
@@ -1,12 +1,22 @@
 import React from 'react'
-
+import Game from './../Game/Game'
 const Generation = ({ generation }) => {
+    const renderGames = (gen) => {
+        return gen.games.map((gen) => (
+            <li>
+                <Game info={gen}/>)
+            </li>
+        )
+    }
     return (
         <div>
             <h1>{generation.region}</h1>
             <p>{generation.description}</p>
             <h3>History</h3>
             <p>{generation.history}</p>
+            <ul>
+                {renderGames(generation)}
+            </ul>
         </div>
     )
 }
