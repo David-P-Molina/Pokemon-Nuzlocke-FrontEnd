50bea5dd6c2a4ffbeb63443bea472729d771b988
David Moina <davemolina777@gmail.com>
2021-12-18 19:27:31 -0600

    Add unique key to types render method

--- src/components/Type/TypesList.js
+++ src/components/Type/TypesList.js
@@ -3,7 +3,7 @@ import Type from './Type'
 import LoadingSpinner from '../LoadingSpinner.js'
 
 const TypesList = ({types}) => {
-    const mapTypes = () => types.map((type) => <Type type={type} />)
+    const mapTypes = () => types.map((type) => <Type key={type.name} type={type} />)
     const renderTypes = () => {
         if (types === []) {
             return <LoadingSpinner content="Co"/>
