723f4d76701524426270227fea93b3a3628e80da
David Moina <davemolina777@gmail.com>
2021-12-18 19:17:08 -0600

    Add geograpy to generation component

--- src/components/Generations/Generation.js
+++ src/components/Generations/Generation.js
@@ -14,6 +14,8 @@ const Generation = ({ generation }) => {
             <p>{generation.description}</p>
             <h3>History</h3>
             <p>{generation.history}</p>
+            <h3>Geography</h3>
+            <p>{generation.geography}</p>
             <ul>
                 {renderGames(generation)}
             </ul>
