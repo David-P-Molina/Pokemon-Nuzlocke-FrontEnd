3d85601457354da0a074b1bfb1601bdff09a7873
David Moina <davemolina777@gmail.com>
2021-12-19 22:41:22 -0600

    Create forEach for generation fetch call

--- src/actions/GenerationAction.js
+++ src/actions/GenerationAction.js
@@ -1,4 +1,5 @@
 import { URL } from '../../src/globalVariables'
+
 export const fetchGenerationsAndGamesAction= () => {
     return (dispatch) => {
         fetch(`${URL}/generations`)
@@ -7,6 +8,9 @@ export const fetchGenerationsAndGamesAction= () => {
                 return response.json()
                 .then((generations) => {
                     debugger
+                    generations.forEach((gen) => {
+                        dispatch({type: 'FETCH_GENERATIONS', generation: gen})
+                    })
                     // dispatch two actions, one for fetch generations and for games
                 })
             } else {
