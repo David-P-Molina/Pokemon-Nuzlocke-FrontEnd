e3da958c623139f6c401bd099595b02fef98ccfb
David Moina <davemolina777@gmail.com>
2022-01-07 12:22:49 -0600

    Create render method for owned and favorit

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,15 +5,18 @@ const Game = ({ game }) => {
 
     const isOwned = () => this.state.owned ? true : false
     const isFavorited = () => this.state.favorited ? true : false
-    
-    const isFavorited = 
+
+    const renderOwned = () => owned === true ? <h1>Owned</h1> : <h1>Not Owned</h1>
+    const renderFavorite = () => favortied === true ? <h5>★</h5> : <h5>☆</h5>
     return (
         <div>
             <aside>
                 <h3>Owned</h3>
-                <button onClick={isOwned}></button>
+                <button onClick={isOwned()}></button>
+                {renderOwned}
                 <h3>Favorite</h3>
-                <button onClick={isFavorited}><3</button>
+                <button onClick={isFavorited()}><3</button>
+                {renderFavorite}
             </aside>
             <h1>{game.name} <em>{game.release_date}</em>></h1> 
             <p>{game.description}</p>
