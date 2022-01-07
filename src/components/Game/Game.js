5125c0ac5cc8d6a98527458c436c514b716b5483
David Moina <davemolina777@gmail.com>
2022-01-07 12:17:29 -0600

    Add basic ternary to change from true to false in state

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 
 const Game = ({ game }) => {
-    const clicked = useState(false)
+    const [owned, favorited] = useState(false)
+
+    const isOwned = () => this.state.owned ? true : false
+    const isFavorited = () => this.state.favorited ? true : false
+    
+    const isFavorited = 
     return (
         <div>
             <aside>
                 <h3>Owned</h3>
+                <button onClick={isOwned}></button>
                 <h3>Favorite</h3>
+                <button onClick={isFavorited}><3</button>
             </aside>
             <h1>{game.name} <em>{game.release_date}</em>></h1> 
             <p>{game.description}</p>
