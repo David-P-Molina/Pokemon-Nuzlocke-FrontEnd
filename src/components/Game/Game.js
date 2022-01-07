0f02a94c8b3cd7a498895ed9b82739fa13c26e29
David Moina <davemolina777@gmail.com>
2022-01-07 12:24:31 -0600

    Format buttons and fix typo

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react'
 
 const Game = ({ game }) => {
-    const [owned, favorited] = useState(false)
+    const [ owned, favorite ] = useState(false)
 
     const isOwned = () => this.state.owned ? true : false
-    const isFavorited = () => this.state.favorited ? true : false
+    const isFavorite = () => this.state.favorite ? true : false
 
     const renderOwned = () => owned === true ? <h1>Owned</h1> : <h1>Not Owned</h1>
-    const renderFavorite = () => favortied === true ? <h5>★</h5> : <h5>☆</h5>
+    const renderFavorite = () => favorite === true ? <h5>★</h5> : <h5>☆</h5>
     return (
         <div>
             <aside>
-                <h3>Owned</h3>
-                <button onClick={isOwned()}></button>
+                <button onClick={isOwned()}>
+                    <h3>Owned</h3>
+                </button>
                 {renderOwned}
-                <h3>Favorite</h3>
-                <button onClick={isFavorited()}><3</button>
+                <button onClick={isFavorite()}>
+                    <h3>Favorite</h3>
+                </button>
                 {renderFavorite}
             </aside>
             <h1>{game.name} <em>{game.release_date}</em>></h1> 
