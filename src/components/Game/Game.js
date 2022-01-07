8580bb8d4d18cb44d401a002c0cb099900da2238
David Moina <davemolina777@gmail.com>
2022-01-07 12:13:20 -0600

    Add useState to game component and create section for owned and favorite

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Game = ({ game }) => {
+    const clicked = useState(false)
     return (
         <div>
+            <aside>
+                <h3>Owned</h3>
+                <h3>Favorite</h3>
+            </aside>
             <h1>{game.name} <em>{game.release_date}</em>></h1> 
             <p>{game.description}</p>
         </div>
